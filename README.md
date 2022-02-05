ac11ee43bbd8a951a66728ddc26ef959c9c4b076
everettjf <everettjf@live.com>
2022-02-05 12:20:11 +0800

    marketplace

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# marketplace
+# ScriptWidget Marketplace
+
+
---
+++ widgets/Image/main.jsx
@@ -0,0 +1,16 @@
+// 
+// ScriptWidget 
+// https://scriptwidget.app
+// 
+// Image Template
+// 
+// Usage: 
+// 1. Add image in Settings->Image
+// 2. Run this script
+// 
+
+$render(
+  <vstack>
+    <image id="image-leaf" />
+  </vstack>
+);
