971ad420c9b6d029a4aebb9337cac63037452c99
everettjf <everettjf@live.com>
2022-02-19 21:06:29 +0800

    add Image No Margin

---
+++ widgets/Image No Margin/main.jsx
@@ -0,0 +1,24 @@
+
+//
+// ScriptWidget
+// https://scriptwidget.app
+//
+//
+
+
+$render(
+  <vstack spacing="0">
+    <zstack>
+        <image id="image0" padding="0" />
+        <text> Hello </text>
+    </zstack>
+    <zstack>
+        <image id="image1" padding="0" />
+        <text> World </text>
+    </zstack>
+    <zstack>
+        <image id="image2" padding="0" />
+        <text> :) </text>
+    </zstack>
+  </vstack>
+);
